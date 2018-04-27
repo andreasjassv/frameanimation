f876664614775dd6b54e46170b946112aba67fd2
Andreas Valdaru <andreasjass.valdaru@tptlive.ee>
2018-04-27 08:41:44 +0300

    Frametoo

---
+++ FrameAnimation/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Graphics.Drawables;
+
+namespace FrameAnimation
+{
+    [Activity(Label = "FrameAnimation", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+        public override void OnWindowFocusChanged(bool hasFocus)
+        {
+            if (hasFocus)
+            {
+                ImageView imageView = FindViewById<ImageView>(Resource.Id.animated_android);
+                AnimationDrawable animation = (AnimationDrawable)imageView.Drawable;
+                animation.Start();
+            }
+        }
+    }
+}
+
